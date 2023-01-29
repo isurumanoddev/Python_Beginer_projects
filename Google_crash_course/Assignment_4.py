18c247417b13545db3998949540b3a5f2799ae33
isurumanoddev <111575402+isurumanoddev@users.noreply.github.com>
2023-01-29 18:28:48 +0530

    Assignment2

--- Google_crash_course/Assignment_4.py
+++ Google_crash_course/Assignment_4.py
@@ -3,4 +3,4 @@ replacement_cycle = 5
 computers_per_year = total_computers/replacement_cycle
 
 
-print(computers_per_year) # Should print 120.0
+print(computers_per_year)
--- other/assignment5.py
+++ other/assignment5.py
@@ -13,16 +13,14 @@ def write_json_to_file(json_obj, output_file):
     with open(output_file, 'w') as outfile:
         # Write json_obj to the new file
         outfile.write(json_obj)
-    # print("write_json_to_file json_obj ",json_obj ,"    :     ", output_file)
 
-    # raise NotImplementedError()
 
 
 def main():
-    # Print the contents of details() -- This should print the details of an employee
+
     details()
 
-    # Create employee dictionary
+
     employee_dict = create_dict(employee_name, age, title)
     print("employee_dict: " + str(employee_dict))
 
@@ -30,9 +28,7 @@ def main():
     Use a function called dumps from the json module to convert employee_dict
     into a json string and store it in a variable called json_object.
     '''
-    ### WRITE YOUR CODE BY MODIFYING THE LINE BELOW
-    # In the line below replace the None keyword with your code.
-    # The format should look like: variable = json.dumps(dict)
+
     json_object = json.dumps(employee_dict)
     print("json_object: " + str(json_object))
 
